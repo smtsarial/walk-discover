b5b0d74984ecf464a329c49b1507da016467c6a6
smtsarial <46844450+smtsarial@users.noreply.github.com>
2021-03-13 01:25:20 +0300

    train section added

--- src/components/train.js
+++ src/components/train.js
@@ -7,21 +7,21 @@ function numberForSource (length) {
   return random
 }
 
-export default class Snow extends React.Component {
+export default class Train extends React.Component {
   state = {
     ytsource: [
-      "https://www.youtube.com/watch?v=CGslieDL-04&ab",
-      "https://www.youtube.com/watch?v=kGJr1Nh-1CY",
-      "https://www.youtube.com/watch?v=ESCNUu4lBE8",
-      "https://www.youtube.com/watch?v=1xG2sizmXkM",
-      "https://www.youtube.com/watch?v=AEdPRquaV_w",
-      "https://www.youtube.com/watch?v=a_oqcg0hvpo",
-      "https://www.youtube.com/watch?v=F8MN0o6RS9o",
-      "https://www.youtube.com/watch?v=jqpc3ak7Rhw",
-      "https://www.youtube.com/watch?v=mECvsV-Ah_8",
-      "https://www.youtube.com/watch?v=ur11-3kBm1A",
-      "https://www.youtube.com/watch?v=4jnyy3G3WIA",
-      "https://www.youtube.com/watch?v=tl8v5f6-zsc"
+      "https://www.youtube.com/watch?v=Mw9qiV7XlFs",
+      "https://www.youtube.com/watch?v=YpcJ6jJlz6o",
+      "https://www.youtube.com/watch?v=3rDjPLvOShM",
+      "https://www.youtube.com/watch?v=Y53k5YCL93c",
+      "https://www.youtube.com/watch?v=9pVWfzsgLoQ",
+      "https://www.youtube.com/watch?v=ADt_RisXY0U",
+      "https://www.youtube.com/watch?v=XsVJ8PCV-0M",
+      "https://www.youtube.com/watch?v=9WxAILsn5-Y",
+      "https://www.youtube.com/watch?v=u70shHkbmWI",
+      "https://www.youtube.com/watch?v=t1_kU54SCcc",
+      "https://www.youtube.com/watch?v=W8PvHerBYaE",
+      "https://www.youtube.com/watch?v=yHuGHrzwlqE"
     ],
   };
 
