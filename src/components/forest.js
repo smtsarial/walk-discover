f63d74c39097aab07fba91f1c6bc875d87568846
smtsarial <46844450+smtsarial@users.noreply.github.com>
2021-04-01 20:05:25 +0300

    new version updated and cities added

--- src/components/forest.js
+++ src/components/forest.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "../styles/App.css";
 import BurgerMenu from "../menu";
 import React from "react";
@@ -35,56 +34,3 @@ export default class App extends React.Component {
     );
   }
 }
-=======
-import "../styles/App.css";
-import BurgerMenu from "../menu";
-import React from "react";
-
- const state ={
-    ytsource: [
-      "https://www.youtube.com/watch?v=hld4uaO1MDE",
-      "https://www.youtube.com/watch?v=b4AVn8mTuJw",
-      "https://www.youtube.com/watch?v=K-Vr2bSMU7o",
-      "https://www.youtube.com/watch?v=h6HLtXxWQMY",
-      "https://www.youtube.com/watch?v=t2NbOcqsgaI",
-      "https://www.youtube.com/watch?v=ynLpZGegiJE",
-      "https://www.youtube.com/watch?v=oSmUI3m2kLk",
-      "https://www.youtube.com/watch?v=S-u6ENAL6sw",
-      "https://www.youtube.com/watch?v=q86990UHsog",
-      "https://www.youtube.com/watch?v=I-zPNQYHSvU",
-      "https://www.youtube.com/watch?v=PyFN_FYwqvc"
-    ]
-  };
-
-const numberForSource = (length) => {
-  const random = Math.floor(Math.random() * length);
-  return random;
-};
-
-const number = numberForSource(state.ytsource.length-1);
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <div className="marquee">
-          <BurgerMenu videoSource={state.ytsource[number]}></BurgerMenu>
-          <div
-            id="ytbg"
-            data-ytbg-fade-in="true"
-            data-ytbg-mute-button="true"
-            data-ytbg-loop="true"
-            data-ytbg-mobile="true"
-            data-youtube={state.ytsource[number]}
-          ></div>
-          <div className="content">
-            <div className="inner">
-              
-            </div>
-          </div>
-        </div>
-      </main>
-    );
-  }
-}
->>>>>>> f7f4487858b62586fdcce4c73a85673f130692b7
--- src/components/train.js
+++ src/components/train.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "../styles/App.css";
 import BurgerMenu from "../menu";
 import React from "react";
@@ -32,57 +31,3 @@ export default class App extends React.Component {
     );
   }
 }
-=======
-import "../styles/App.css";
-import BurgerMenu from "../menu";
-import React from "react";
-
- const state ={
-    ytsource: [
-      "https://www.youtube.com/watch?v=Mw9qiV7XlFs",
-      "https://www.youtube.com/watch?v=YpcJ6jJlz6o",
-      "https://www.youtube.com/watch?v=3rDjPLvOShM",
-      "https://www.youtube.com/watch?v=Y53k5YCL93c",
-      "https://www.youtube.com/watch?v=9pVWfzsgLoQ",
-      "https://www.youtube.com/watch?v=ADt_RisXY0U",
-      "https://www.youtube.com/watch?v=XsVJ8PCV-0M",
-      "https://www.youtube.com/watch?v=9WxAILsn5-Y",
-      "https://www.youtube.com/watch?v=u70shHkbmWI",
-      "https://www.youtube.com/watch?v=t1_kU54SCcc",
-      "https://www.youtube.com/watch?v=W8PvHerBYaE",
-      "https://www.youtube.com/watch?v=yHuGHrzwlqE"
-    ]
-  };
-
-const numberForSource = (length) => {
-  const random = Math.floor(Math.random() * length);
-  return random;
-};
-
-const number = numberForSource(state.ytsource.length-1);
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <main>
-        <div className="marquee">
-          <BurgerMenu videoSource={state.ytsource[number]}></BurgerMenu>
-          <div
-            id="ytbg"
-            data-ytbg-fade-in="true"
-            data-ytbg-mute-button="true"
-            data-ytbg-loop="true"
-            data-ytbg-mobile="true"
-            data-youtube={state.ytsource[number]}
-          ></div>
-          <div className="content">
-            <div className="inner">
-              
-            </div>
-          </div>
-        </div>
-      </main>
-    );
-  }
-}
->>>>>>> f7f4487858b62586fdcce4c73a85673f130692b7
